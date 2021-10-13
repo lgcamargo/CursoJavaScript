c65f4835fcc7333fd6f5d843ca308a7a0fc8d3e6
Lucas Camargo <69404041+lgcamargo@users.noreply.github.com>
2021-10-12 23:12:04 -0300

    model

---
+++ aula26/assets/js/main.js
@@ -0,0 +1,7 @@
+// receber o submit
+const form = document.querySelector('#formulario');
+
+form.addEventListener('submit', function (e) {
+ e.preventDefault();
+ console.log('evento previnido')
+});
